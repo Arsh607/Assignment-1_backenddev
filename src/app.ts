1b449f5fb1dfd230d449b51b3e5787ecde0d6bec
rrc_f2024_arishi <arishi@academic.rrc.ca>
2026-01-18 19:30:18 -0600

    feat: Create the health checkpoint.

--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,13 @@ import express, { Express, Request, Response } from "express";
 
 export const app: Express = express(); //changed to a name export because default export was giving an error
 
+//Health checkpoint
+app.get("/health", (req: Request, res: Response) => {
+    res.json({
+        status: "OK",
+        uptime: process.uptime(),
+        timestamp: new Date().toISOString(),
+        version: "1.0.0",
+    })
+})
 //export default app;
